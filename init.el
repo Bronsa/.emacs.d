e0ede4cfdf4c40cfd9586cae2448d8737d078c69
Nicola Mometto <brobronsa@gmail.com>
2016-01-11 14:29:43 +0000

    update

--- init.el
+++ init.el
@@ -18,10 +18,18 @@
 (load-dotfile "config/core.el")
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-modules
+   (quote
+    (autojoin track completion irccontrols log netsplit match noncommands readonly stamp autoaway log scrolltobottom fill truncate ring networks move-to-prompt)))
  '(magit-diff-section-arguments (quote ("--no-ext-diff")))
  '(safe-local-variable-values
    (quote
-    ((eval define-clojure-indent
+    ((css-indent-offset . 2)
+     (eval define-clojure-indent
            (mlet 1)
            (lazy-seq 0)
            (match 1)
@@ -31,3 +39,9 @@
      (cider-refresh-before-fn . "bounce.core/stop!")
      (nameless-current-name . "cider")
      (nameless-affect-indentation-and-filling)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
