cd348b4dad08881b6b1b407c076fd326708f58a8
Nicola Mometto <brobronsa@gmail.com>
2018-03-30 16:01:23 +0200

    update

--- config/erc.el
+++ config/erc.el
@@ -26,7 +26,7 @@
 
       erc-prompt-for-nickserv-password nil
 
-      erc-autojoin-channels-alist '(("freenode.net" "#clojure" "#metamorphism"))
+      erc-autojoin-channels-alist '(("freenode.net" "#clojure" "#shen" "#lisp" "#ocaml"))
 
       erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "324" "329" "332" "333" "353" "477")
       erc-hide-list '()
